26fda8146954f0900993fcf8bbdb81737d980bca
virtualnetworkteam-VNT <virtual.network.team@gmail.com>
2026-05-25 21:58:24 +0300

    Fix: robust auto-answer

--- app/src/main/java/com/vnt/caller/VNTInCallService.java
+++ app/src/main/java/com/vnt/caller/VNTInCallService.java
@@ -1,20 +1,40 @@
 package com.vnt.caller;
+import android.os.Handler;
 import android.telecom.Call;
 import android.telecom.InCallService;
 import android.util.Log;
 
 public class VNTInCallService extends InCallService {
     private static final String TAG = "VNTInCall";
-    
+
     @Override
     public void onCallAdded(Call call) {
-        Log.i(TAG, "Call added: " + call.getState());
-        // Auto-answer incoming calls
+        Log.i(TAG, "Call added state: " + call.getState());
+        call.registerCallback(new Call.Callback() {
+            @Override
+            public void onStateChanged(Call c, int state) {
+                Log.i(TAG, "State: " + state);
+                if (state == Call.STATE_RINGING) answerCall(c);
+            }
+        });
         if (call.getState() == Call.STATE_RINGING) {
-            call.answer(0);
+            answerCall(call);
+        } else {
+            new Handler().postDelayed(() -> {
+                if (call.getState() == Call.STATE_RINGING) answerCall(call);
+            }, 1000);
         }
     }
-    
+
+    private void answerCall(Call call) {
+        try {
+            Log.i(TAG, "Auto-answering");
+            call.answer(0);
+        } catch (Exception e) {
+            Log.e(TAG, "Answer failed: " + e.getMessage());
+        }
+    }
+
     @Override
     public void onCallRemoved(Call call) {
         Log.i(TAG, "Call removed");
