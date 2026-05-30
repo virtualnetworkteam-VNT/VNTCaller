c6e1fa2e601dad9208c37659a4aeb52c3be19f2b
virtualnetworkteam-VNT <virtual.network.team@gmail.com>
2026-05-30 10:29:48 +0300

    done

--- commands/vbrain.py
+++
@@ -1,7 +0,0 @@
-
-import urllib.request,json,base64
-relay=open("/home/k/github-relay.py").read()
-GH=relay.split('GH="')[1].split('"')[0] if 'GH="' in relay else relay.split("GH='")[1].split("'")[0]
-req=urllib.request.Request("https://api.github.com/repos/virtualnetworkteam-VNT/VNTCaller/contents/assets/verify_brain.py",headers={"Authorization":"Bearer "+GH})
-with urllib.request.urlopen(req,timeout=15) as r:
-    exec(base64.b64decode(json.loads(r.read())["content"]).decode())
